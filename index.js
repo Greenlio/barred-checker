88ebb718fe0473a822ccea51683a2de6b2ef8bf4
Greenlio <62725108+Greenlio@users.noreply.github.com>
2021-10-06 00:38:35 -0500

    hey this is just cooler in general

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const { Minehut } = require('minehut')
-
+const { Server } = require('config.json')
 // i know the code is messy stop screaming at me i just started making this
 async function mainFunction() {
 	const minehut = new Minehut();
-	const server = await minehut.servers.get('Barred', true);
+	const server = await minehut.servers.get(Server, true);
     //let's get organized, then ill go to bed.
     
     // is barred online?
-    console.log('Is Barred online: ' + server.online)
+    console.log(`Is ${Server} online: ` + server.online)
     // how many people can barred have
-    console.log('How many people can Barred have: ' + server.maxPlayers)
+    console.log(`How many people can ${Server} have: ` + server.maxPlayers)
     // how many online
-    console.log('How many are on Barred: ' + server.playerCount)
+    console.log(`How many are on ${Server}: ` + server.playerCount)
 };
 
 mainFunction()
