a21fc40a3835a7104a61a1d5de1f9273043d862b
Greenlio <62725108+Greenlio@users.noreply.github.com>
2021-10-06 00:36:10 -0500

    i need to sleep

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const { Minehut } = require('minehut')
 
 // i know the code is messy stop screaming at me i just started making this
-async function mainfunction() {
+async function mainFunction() {
 	const minehut = new Minehut();
 	const server = await minehut.servers.get('Barred', true);
-    console.log(server.playerCount)
+    //let's get organized, then ill go to bed.
+    
+    // is barred online?
+    console.log('Is Barred online: ' + server.online)
+    // how many people can barred have
+    console.log('How many people can Barred have: ' + server.maxPlayers)
+    // how many online
+    console.log('How many are on Barred: ' + server.playerCount)
 };
 
 mainFunction()
