6ee3729f89b08ffcb93ef3e02074adc5e1aae1e8
Greenlio <62725108+Greenlio@users.noreply.github.com>
2021-10-06 00:31:12 -0500

    so i dont accidentally crash my pc

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const { Minehut } = require('minehut')
 
 // i know the code is messy stop screaming at me i just started making this
-async function myFunction() {
+async function mainfunction() {
 	const minehut = new Minehut();
 	const server = await minehut.servers.get('Barred', true);
-
-	const plugins = await server.getInstalledContent();
-	const pluginsmap = plugins.map(p => p.title)
-    console.log(pluginsmap)
-
-    if (pluginsmap.includes('Amongus')) {
-        console.log('true')
-    } else {
-        console.log('no')
-    }
+    console.log(server.playerCount)
 };
 
-myFunction()
+mainFunction()
