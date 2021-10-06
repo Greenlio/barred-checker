fde612262597308b92469cba4a570a0dc768b227
Greenlio <62725108+Greenlio@users.noreply.github.com>
2021-10-06 00:26:04 -0500

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Minehut } = require('minehut')
 
-
+// i know the code is messy stop screaming at me i just started making this
 async function myFunction() {
 	const minehut = new Minehut();
 	const server = await minehut.servers.get('Barred', true);
